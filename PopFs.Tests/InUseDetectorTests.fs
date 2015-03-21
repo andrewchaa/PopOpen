a232c975e25688fbbffd382ceae21c4ee64e1790
Andrew <andrew.chaa@yahoo.co.uk>
2015-03-21 13:25:05 +0000

    added text test

---
+++ PopFs.Tests/InUseDetectorTests.fs
@@ -0,0 +1,21 @@
+﻿namespace PopFs.Tests
+
+open Pop.Cs
+open Pop
+open NUnit.Framework
+open FsUnit
+open System
+open System.IO
+open System.Threading
+
+[<TestFixture>]
+type ``Given InUseDetection`` ()=
+    let openFile = 
+        let file = Path.Combine(Environment.CurrentDirectory, "IAmTextFile.txt")
+        System.Diagnostics.Process.Start (file) |> ignore
+        Thread.Sleep(2000)
+        InUseDetection.GetProcessesUsingFiles [file]
+
+    [<Test>] member t.
+        ``When it opens a text file`` ()=
+            openFile |> should haveCount 1
