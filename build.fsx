c0d0b1a80367755f70a6835819b886d35c7ff70f
andrewchaa <andrew.chaa@huddle.com>
2015-03-31 12:11:51 +0100

    build version change

--- build.fsx
+++ build.fsx
@@ -36,7 +36,7 @@ Target "CreatePackage" (fun _ ->
         { 
         p with 
             Authors = ["Andrew Chaa"]
-            Version = "0.8.2.2"
+            Version = "0.8.3.0"
             NoPackageAnalysis = true
             ToolPath = @".\Nuget.exe" 
             OutputPath = nugetDir
