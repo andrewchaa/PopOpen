3330d27e3cc25a7400c3534f87a3de7726844a79
Andrew <andrew.chaa@yahoo.co.uk>
2015-04-06 12:36:35 +0100

    0.8.4.2 version

--- build.fsx
+++ build.fsx
@@ -44,7 +44,7 @@ Target "CreatePackage" (fun _ ->
         { 
         p with 
             Authors = ["Andrew Chaa"]
-            Version = "0.8.4.1"
+            Version = "0.8.4.2"
             NoPackageAnalysis = true
             ToolPath = @".\Nuget.exe" 
             OutputPath = nugetDir
